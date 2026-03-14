3a95abe91fb1008a81daff534e8ed6dfae7eec77
djumanov <djumanovdev@gmail.com>
2026-03-14 09:06:24 +0500

    custom auth user model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'chat_id', 'telegram_username')
+    search_fields = ('username', 'chat_id', 'telegram_username')
--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    chat_id = models.CharField(max_length=255, unique=True)
+    telegram_username = models.CharField(max_length=255, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.username
+    
